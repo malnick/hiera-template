818d2bd0a0d818a79ff640a88d452d37bb1e4179
Jeff <malnick@gmail.com>
2014-10-24 15:46:15 -0700

    more stubbing

--- lib/template.rb
+++ lib/template.rb
@@ -1,10 +1,28 @@
 require 'yaml'
+require './config'
 
 
 module template
+	class create
+		attr_accessor :profile_path
+		# Grab hiera data path from config
+		@hiera_data_path = Config.get_hiera_data_path
+		@keys = []
 
-	def Template.create(path) 
+		def create_new_template(:new_template_path)
 
+			new_file = File.new(:new_template_path, 'w')
+			puts "Writing the new template to: ", :new_template_path
+			@keys.each do | key | 
+				new_file.write(key)
+			end
+			new_file.close
+		end
+
+		def Self.get_data_from_profile(:profile_path)
+			# Parse the profile for all hiera() data keys. 			
+		end
 	end
+
 
 end
