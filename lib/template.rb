d7d5b29b73db144097a3252597f13ee85057f06b
Jeff <malnick@gmail.com>
2014-10-27 09:40:51 -0700

    adding logger statements

--- lib/template.rb
+++ lib/template.rb
@@ -1,19 +1,21 @@
 require 'yaml'
-#require './config'
+require 'logger'
+$LOAD_PATH << '.'
 
 module Template
 	class Create
+		#include Extras 
+		@log	= Logger.new(STDOUT) 
 
 		def initialize(new_template_path, profile_path)
-		#	unless new_template_path.absolute? && profile_path.absolute? 
-		#		abort "New template & profile path must be absolute"
-		#	end
 			@new_template_path 	= new_template_path
 			@profile_path 		= profile_path
-			#@hiera_data_path 	= Config.get_hiera_data_path
+#			@hiera_data_path 	= Extras.get_hiera_data_path
 			@keys 			= Hash.new 
+					puts "###"
 			puts "Creating new template for #{@profile_path}"
-			puts "New template path: #{@new_template_path}"
+			puts "New template path #{@new_template_path}"
+			puts "###"
 			parse()
 			write()
 		end
@@ -41,10 +43,31 @@ module Template
 			template = File.open(@new_template_path, 'w')
 			@keys.to_hash
 			template.write(@keys.to_yaml)
-			#@keys.each do |k|
-			#	template.write(k.to_yaml)
-			#end
 			template.close
 		end
+
+		def verify(path)
+			unless path.absolute?
+				error("Path #{path} must be absolute")
+			end
+		end
+
+		def error(message)
+			logger.error("Fatal error occured: #{message}")
+			abort 
+		end
+
+		def debug(message)
+			logger.debug("DEBUG: ", message)
+		end
+
+		def warn(message)
+			logger.warn("WARNING: ", message)
+		end
+
+		def info(message)
+			logger.info("INFO: ", message)
+		end
+
 	end
 end
