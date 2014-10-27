a88e18d5f5ee3997f8713982777bbe3676144677
Jeff <malnick@gmail.com>
2014-10-27 06:51:38 -0700

    parsing mock profile for hiera data

--- bin/template.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'template'
-
-template = Template::Create.new
-template.create_new_template(ARGV[1])
--- lib/template.rb
+++ lib/template.rb
@@ -1,30 +1,53 @@
 require 'yaml'
-require './config'
+#require './config'
 
 
-module template
-	class create
-		attr_accessor :profile_path
-		attr_accessor :new_template_path
-		# Grab hiera data path from config
-		@hiera_data_path = Config.get_hiera_data_path
-		@keys = []
+module Template
+	class Create
 
-		def create(:new_template_path)
+		def initialize(new_template_path, profile_path)
 
-			new_file = File.new(:new_template_path, 'w')
-			puts "Writing the new template to: ", :new_template_path
-			@keys.each do | key | 
-				new_file.write(key)
-			end
-			new_file.close
+		#	unless new_template_path.absolute? && profile_path.absolute? 
+		#		abort "New template & profile path must be absolute"
+		#	end
+			
+			@new_template_path 	= new_template_path
+			@profile_path 		= profile_path
+			#@hiera_data_path 	= Config.get_hiera_data_path
+			@keys 			= []
+			puts "Creating new template for #{@profile_path}"
+			puts "New template path: #{@new_template_path}"
+			parse()
+			#create(@new_template_path)
 		end
 
-		def Self.get_data_from_profile(:profile_path)
-			# Parse the profile for all hiera() data keys. 			
-			# Add keys to @keys
+		def parse
+			profile = File.open(@profile_path)
+			profile.each_line do |line|
+				if line.match(/hiera/)
+					data = line.split("(").last
+					data.delete! (")")
+					data.delete! ("\"")
+					data.delete! ("'")
+					puts "Data: #{data}"
+				end
+
+			end
+
 		end
 	end
+		#def self.(er:new_template_path)
 
+		#	new_file = File.new(:new_template_path, 'w')
+		#	puts "Writing the new template to: ", :new_template_path
+		#	@keys.each do | key | 
+		#		new_file.write(key)
+		#	end
+		#	new_file.close
+		#end
 
+		#def Self.get_data_from_profile(:profile_path)
+		#	# Parse the profile for all hiera() data keys. 			
+		#	# Add keys to @keys
+		#end
 end
